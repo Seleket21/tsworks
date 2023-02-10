61603b462a142528f17b38891456f83b011e49c6
root <Melvin Mickle>
2023-02-10 14:40:08 +0530

    first commit

--- data.py
+++ data.py
@@ -1,44 +1,15 @@
-from flask import Flask, request
-import sqlite3
+import csv
+import requests
 
-app = Flask(__name__)
+def download_csv(ticker):
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1=1644314526&period2=1675850526&interval=1d&events=history&includeAdjustedClose=true"
+    response = requests.get(url, headers = user_agent)
+    with open(f"{ticker}.csv", 'w') as f:
+        f.write(response.text)
 
-@app.route('/companies/<date>', methods=['GET'])
-def get_companies_data(date):
-    conn = sqlite3.connect('stocks.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM stocks WHERE date=?", (date,))
-    stocks = c.fetchall()
-    conn.close()
-    return str(stocks)
+with open('tickers.txt') as f:
+    tickers = f.read().splitlines()
 
-@app.route('/company/<company_name>/<date>', methods=['GET'])
-def get_company_data(company_name, date):
-    conn = sqlite3.connect('stocks.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM stocks WHERE company=? AND date=?", (company_name, date))
-    stock = c.fetchone()
-    conn.close()
-    return str(stock)
-
-@app.route('/company/<company_name>', methods=['GET'])
-def get_company_all_data(company_name):
-    conn = sqlite3.connect('stocks.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM stocks WHERE company=?", (company_name,))
-    stocks = c.fetchall()
-    conn.close()
-    return str(stocks)
-
-@app.route('/update_data', methods=['POST'])
-def update_data():
-    company = request.form['company']
-    date = request.form['date']
-    value = request.form['value']
-    conn = sqlite3.connect('stocks.db')
-    c = conn.cursor()
-    c.execute("UPDATE stocks SET value=? WHERE company=? AND date=?", (value, company, date))
-    conn.commit()
-    conn.close()
-    return "Data updated successfully"
-
+for ticker in tickers:
+    download_csv(ticker)
